d3479e1e44fec4d6b0a4af6d18d451b1a45b1ed5
原田裕太 <haradayuuta@haradayuutanoMacBook-Air.local>
2024-10-31 06:36:53 +0900

    fix:サインアップの機能に必要なファイルを変更・削除

--- src/main/java/business/dto/users/SignupUserResultDTO.java
+++ src/main/java/business/dto/users/SignupUserResultDTO.java
@@ -1,5 +1,32 @@
-package business.dto;
+package business.service.users;
 
+import entity.User;
+
+/**
+ * {@link SignupUser#execute(SignUpForm)}の結果の出力に用いるオブジェクト。
+ * 登録されたユーザーの情報を表すエンティティオブジェクトと登録結果のメッセージをまとめる。
+ */
 public class SignupUserResultDTO {
-    
+    /**
+     * 登録されたユーザーの情報を表すエンティティオブジェクト。
+     * 処理結果の出力にのみ使用され、変更の必要がないためfinal修飾子を付与している。
+     */
+    public final User user;
+
+    /**
+     * ユーザー登録の結果を表すメッセージ。
+     * 処理結果の出力にのみ使用され、変更の必要がないためfinal修飾子を付与している。
+     */
+    public final String message;
+
+    /**
+     * コンストラクタ。
+     * 
+     * @param user 登録されたユーザーの情報
+     * @param message 登録結果のメッセージ
+     */
+    public SignupUserResultDTO(User user, String message) {
+        this.user = user;
+        this.message = message;
+    }
 }
