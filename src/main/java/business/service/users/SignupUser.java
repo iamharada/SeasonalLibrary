1bb35a87de1f961437f80f54555449fe778fe25d
原田裕太 <haradayuuta@haradayuutanoMacBook-Air.local>
2024-10-31 05:58:42 +0900

    fix:サインアップの機能を変更

--- src/main/java/business/service/users/SignupUser.java
+++ src/main/java/business/service/users/SignupUser.java
@@ -1,5 +1,66 @@
 package business.service.users;
 
+import dao.DaoException;
+import dao.UserDao;
+import entity.User;
+import modelUtil.Failure;
+
+/**
+ * ユーザー登録を行うビジネスロジッククラス。
+ */
 public class SignupUser {
-    
+    /**
+     * ユーザー登録を実行する。
+     * 
+     * @param form 登録するユーザーの情報を{@link SignUpForm}型のオブジェクトにまとめたものを入力とする。
+     * @return 登録に成功した場合は、{@link SignupUserResultDTO}型のオブジェクトを返す。
+     * @throws Failure 登録に失敗した場合は、{@link Failure}型の例外を投げる。
+     */
+    public SignupUserResultDTO execute(SignUpForm form) throws Failure {
+        try {
+            // 入力値の検証
+            validate(form);
+
+            // 入力の情報を用いて、ユーザー情報のエンティティオブジェクトをインスタンス化する
+            User user = new User(form.getName(), form.getEmail(), form.getPassword());
+
+            // DAOをインスタンス化する
+            UserDAO userDao = new UserDAO();
+
+            // ユーザー情報のエンティティオブジェクトをDAOに渡して登録する
+            userDAO.create(user);
+
+            // 登録に成功したことを意味する情報を返す
+            SignupUserResultDTO result = new SignupUserResultDTO(user.getName(), user.getEmail());
+            return result;
+
+        } catch (Failure failure) {
+            // バリデーションエラーやユーザー作成時の例外を処理
+            throw new Failure("ユーザー登録に失敗しました：" + failure.getMessage(), failure);
+
+        } catch (DaoException daoException) {
+            // DAOでの例外を処理
+            throw new Failure("ユーザー登録に失敗しました：" + daoException.getMessage(), daoException);
+        }
+    }
+
+    /**
+     * 入力値の検証を行う。
+     * 
+     * @param form 検証する入力フォーム
+     * @throws Failure 検証に失敗した場合
+     */
+    private void validate(SignUpForm form) throws Failure {
+        UserDAO userDAO = new UserDAO();
+
+        // メールアドレスの重複チェック
+        if (userDAO.findByEmail(form.getEmail())) {
+            throw new Failure("このメールアドレスは既に登録されています");
+        }
+
+        // パスワード一致チェック
+        if (!form.getPassword().equals(form.getConfirmPassword())) {
+            throw new Failure("パスワードと確認用パスワードが一致しません");
+        }
+    }
 }
--- src/main/java/dataaccess/dao/UserDao.java
+++ src/main/java/dataaccess/dao/UserDao.java
@@ -1,3 +1,56 @@
+package dataaccess.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import entity.User;
+
+/**
+ * ユーザー情報のデータベースアクセスを行うDAO。
+ */
 public class UserDao {
-    
+    /**
+     * ユーザー情報を新規登録する。
+     *
+     * @param user 登録するユーザー情報
+     * @throws DaoException データベースアクセスに失敗した場合
+     */
+    public void create(User user) throws DaoException {
+        try (Connection con = DBManager.getConnection();
+             PreparedStatement pstmt = con.prepareStatement(
+                "INSERT INTO users (name, email, password) VALUES (?, ?, ?)")) {
+            
+            pstmt.setString(1, user.getName());
+            pstmt.setString(2, user.getEmail());
+            pstmt.setString(3, user.getPassword());
+            pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new DaoException("ユーザーの登録に失敗しました", e);
+        }
+    }
+
+    /**
+     * 指定されたメールアドレスのユーザーが存在するかチェックする。
+     *
+     * @param email チェックするメールアドレス
+     * @return ユーザーが存在する場合はtrue、存在しない場合はfalse
+     * @throws DaoException データベースアクセスに失敗した場合
+     */
+    public boolean findByEmail(String email) throws DaoException {
+        try (Connection con = DBManager.getConnection();
+             PreparedStatement pstmt = con.prepareStatement(
+                "SELECT COUNT(*) FROM users WHERE email = ?")) {
+            
+            pstmt.setString(1, email);
+            try (ResultSet rs = pstmt.executeQuery()) {
+                rs.next();
+                return rs.getInt(1) > 0;
+            }
+
+        } catch (SQLException e) {
+            throw new DaoException("メールアドレスの重複チェックに失敗しました", e);
+        }
+    }
 }
