558def03bc1838d1c9a7c6e4465bdbf8eebf995f
原田裕太 <haradayuuta@haradayuutanoMacBook-Air.local>
2024-10-31 04:44:45 +0900

    add:サインアップに必要なファイルを追加

---
+++ src/main/java/presentation/form/users/SignupUserForm.java
@@ -0,0 +1,42 @@
+package presentation.form.users;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SignUpUserForm {
+    // このクラスから作成されたオブジェクトはコントロールへの入力の際に一度用いられるのみなので、各フィールドが変更可能である必要はない。
+    // そこで、`private`修飾子付きのフィールドとゲッターメソッドを定義する代わりに、
+    // 各フィールドに`final`修飾子を付けることで、読み取り専用の変更不可能なフィールドを定義している。
+
+    public final String name;
+    public final String email;
+    public final String password;
+    public final String confirmPassword;
+
+    // コンストラクタ
+    public SignUpForm(String name, String email, String password, String confirmPassword) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.confirmPassword = confirmPassword;
+    }
+
+    // Getter methods
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+}
